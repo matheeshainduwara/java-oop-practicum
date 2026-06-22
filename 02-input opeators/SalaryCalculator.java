cdbc43e7b6e4ca4a4ac13b50c683ca420aa75840
Matheesha Induwara <matheesha000@gmail.com>
2026-06-22 20:31:29 +0530

    Add extra tax into salary calcutator exercise

--- 02-input opeators/SalaryCalculator.java
+++ 02-input opeators/SalaryCalculator.java
@@ -5,6 +5,7 @@ Input:
 -Basic Salary
 -Allowance
 -Overtime Hours
+-Tax 5%
 
 Calculate final salary. 
 */
@@ -14,19 +15,30 @@ import java.util.Scanner;
 public class SalaryCalculator {
     public static void main(String[] args) {
         
+        double otrate=450;
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Enter Basic Salary: ");
         double basic = scanner.nextDouble();
-        System.out.println("Enter Allowance(Per Hour): ");
+        System.out.println("Enter Allowance: ");
         double allowance = scanner.nextDouble();
         System.out.println("Overtime Hours: ");
         double othours = scanner.nextDouble();
 
-        double otpayment=(allowance*othours);
-        double finalSalary=(basic+otpayment);
+        double otpayment=(otrate*othours);
+        double finalSalary=(basic+allowance+otpayment);
+        double tax=(finalSalary*5)/100;
+        double netSalary=(finalSalary-tax);
 
-        System.out.println("Final Salary: "+finalSalary);
+        
+
+        System.out.println("Salary Report");
+        System.out.println("-------------");
+        System.out.println("Basic Salary: "+basic);
+        System.out.println("Allowance: "+allowance);
+        System.out.println("Overtime Pay: "+otpayment);
+        System.out.println("Tax: "+tax);
+        System.out.println("Net Salary: "+netSalary);
         
 
     }
